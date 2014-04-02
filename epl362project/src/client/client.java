adf76fd3a51ca18f5cc7236fa4b517abd994f84e
mchris23 <mchris23@cs.ucy.ac.cy>
2014-04-02 16:33:36 +0300

    Implemented client's side for client-server communication & user-pass verification

--- epl362project/src/client/client.java
+++ epl362project/src/client/client.java
@@ -3,20 +3,87 @@ package client;
 import java.io.*;
 import java.net.*;
 
-class client
-{
- public static void main(String argv[]) throws Exception
- {
-  String sentence;
-  String modifiedSentence;
-  BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-  Socket clientSocket = new Socket("localhost", 6789);
-  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-  sentence = inFromUser.readLine();
-  outToServer.writeBytes(sentence + '\n');
-  modifiedSentence = inFromServer.readLine();
-  System.out.println("FROM SERVER: " + modifiedSentence);
-  clientSocket.close();
- }
+class client {
+
+	private static Socket sock;
+
+	public static int verifyUser(String username, String password)
+			throws IOException {
+		if (!openSocket())
+			throw new IOException();
+		if (!send("VERIFY", username, password))
+			throw new IOException();
+		String serverResponse = receive();
+		int retVal;
+		try {
+			retVal = Integer.parseInt(serverResponse); // Server should send one
+														// int as response in
+														// this case
+
+		} catch (NumberFormatException e) {
+			throw new IOException();
+		}
+		closeSocket();
+		return retVal;
+	}
+
+	private static boolean openSocket() {
+		try {
+			sock = new Socket("localhost", 6789);
+		} catch (UnknownHostException e) {
+			System.err.println("Unable to resolve host:");
+			System.err.println(e.getStackTrace());
+			return false;
+		} catch (IOException e) {
+			System.err.println("Unable to create socket:");
+			System.err.println(e.getStackTrace());
+			return false;
+		}
+		return true;
+	}
+
+	private static void closeSocket() {
+		if (!sock.isClosed())
+			try {
+				sock.close();
+			} catch (IOException e) {
+				System.err.println("Unable to close socket:");
+				System.err.println(e.getStackTrace());
+			}
+	}
+
+	private static boolean send(String... data) {
+		if (!sock.isConnected())
+			return false;
+		try {
+			DataOutputStream outToServer = new DataOutputStream(
+					sock.getOutputStream());
+			for (String i : data)
+				outToServer.writeBytes(i + ' ');
+		} catch (IOException e) {
+			System.err.println("Unable to send data to server:");
+			System.err.println(e.getStackTrace());
+			return false;
+		}
+		return true;
+	}
+
+	private static String receive() {
+		if (!sock.isConnected())
+			return null;
+		String ret = new String();
+		String temp = new String();
+		try {
+			BufferedReader inFromServer = new BufferedReader(
+					new InputStreamReader(sock.getInputStream()));
+			while ((temp = inFromServer.readLine()) != null)
+				ret = ret + temp + '\n';
+		} catch (IOException e) {
+			System.err.println("Unable to read data from server:");
+			System.err.println(e.getStackTrace());
+			return null;
+		}
+		return ret;
+
+	}
 }
