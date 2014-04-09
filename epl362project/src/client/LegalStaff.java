c08dd4c8fd577ac2b5cedfbc5408ad58cc8a21b8
Andreas91 <andreasandreou1991@hotmail.com>
2014-04-09 03:54:45 +0300

    Legal Staff Update

--- epl362project/src/client/LegalStaff.java
+++ epl362project/src/client/LegalStaff.java
@@ -15,10 +15,25 @@ import java.awt.Font;
 import javax.swing.JSeparator;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JTextArea;
 
 public class LegalStaff extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtSearch;
+	private JTextField inName;
+	private JTextField inSurname;
+	private JCheckBox chckbxFlag;
+	private JButton btnSaveName;
+	private JButton btnEditName;
+	
 
 	/**
 	 * Create the frame.
@@ -28,7 +43,7 @@ public class LegalStaff extends JFrame {
 		// Create Frame
 		setTitle("Legal Staff");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 485);
+		setBounds(100, 100, 520, 794);
 		
 		// Create Menu Bar
 		JMenuBar menuBar = new JMenuBar();
@@ -80,8 +95,124 @@ public class LegalStaff extends JFrame {
 		
 		// Line Separator
 		JSeparator separator = new JSeparator();
-		separator.setBounds(0, 84, 432, 2);
+		separator.setBounds(0, 83, 502, 3);
 		contentPane.add(separator);
+		
+		// Search Field
+		txtSearch = new JTextField();
+		txtSearch.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtSearch.setText("search client...");
+		txtSearch.setBounds(10, 110, 455, 44);
+		contentPane.add(txtSearch);
+		txtSearch.setColumns(10);
+		
+		// Search Button
+		JButton btnNewButton = new JButton("Search");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnNewButton.setBounds(12, 167, 97, 25);
+		contentPane.add(btnNewButton);
+		
+		// Line Separator
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(0, 205, 502, 3);
+		contentPane.add(separator_1);
+		
+		// Name Label
+		JLabel lblName = new JLabel("Name:");
+		lblName.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblName.setBounds(5, 281, 59, 30);
+		contentPane.add(lblName);
+		
+		// Name TextField		
+		inName = new JTextField();
+		inName.setEditable(false);
+		inName.setText("Client's Name");
+		inName.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		inName.setBounds(116, 278, 216, 37);
+		contentPane.add(inName);
+		inName.setColumns(10);
+		
+		// Surname Label
+		JLabel lblSurname = new JLabel("Surname:");
+		lblSurname.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblSurname.setBounds(5, 321, 99, 30);
+		contentPane.add(lblSurname);
+				
+		// Surname TextField
+		inSurname = new JTextField();
+		inSurname.setText("Client's Surname");
+		inSurname.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		inSurname.setEditable(false);
+		inSurname.setColumns(10);
+		inSurname.setBounds(116, 318, 216, 37);
+		contentPane.add(inSurname);
+		
+		// Flag Label
+		JLabel lblFlag = new JLabel("Flag:");
+		lblFlag.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblFlag.setBounds(7, 364, 59, 30);
+		contentPane.add(lblFlag);
+		
+		// Flag CheckBox
+		chckbxFlag = new JCheckBox("Yes");
+		chckbxFlag.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		chckbxFlag.setSelected(true);
+		chckbxFlag.setEnabled(false);
+		chckbxFlag.setBounds(116, 367, 113, 25);
+		contentPane.add(chckbxFlag);
+		
+		// Edit Button
+		btnEditName = new JButton("Edit");
+		btnEditName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				inName.setEditable(!inName.isEditable());
+				inSurname.setEditable(!inSurname.isEditable());
+				chckbxFlag.setEnabled(!chckbxFlag.isEnabled());
+				btnSaveName.setEnabled(!btnSaveName.isEnabled());
+				if (btnEditName.getText().equals("Edit"))
+					btnEditName.setText("Cancel");
+				else
+					btnEditName.setText("Edit");
+			}
+		});
+		btnEditName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnEditName.setBounds(113, 221, 108, 25);
+		contentPane.add(btnEditName);
+		
+		// Save Button
+		btnSaveName = new JButton("Save");
+		btnSaveName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				inName.setEditable(false);
+				inSurname.setEditable(false);
+				chckbxFlag.setEnabled(false);
+				btnSaveName.setEnabled(false);
+				btnEditName.setText("Edit");
+			}
+		});
+		btnSaveName.setEnabled(false);
+		btnSaveName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnSaveName.setBounds(233, 221, 97, 25);
+		contentPane.add(btnSaveName);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(0, 408, 502, 3);
+		contentPane.add(separator_2);
+		
+		JLabel lblComment = new JLabel("Comments:");
+		lblComment.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblComment.setBounds(5, 419, 99, 30);
+		contentPane.add(lblComment);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Monospaced", Font.PLAIN, 14));
+		textArea.setBounds(10, 462, 480, 197);
+		contentPane.add(textArea);
+		
+		JButton btnAddComment = new JButton("Add Comment");
+		btnAddComment.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnAddComment.setBounds(10, 672, 185, 37);
+		contentPane.add(btnAddComment);
 		
 	}
 }
