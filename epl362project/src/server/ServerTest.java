e17596ade4f13793380cc8a27786ddaadb41f07d
mchris23 <mchris23@cs.ucy.ac.cy>
2014-05-07 02:39:47 +0300

    JUnit for server

---
+++ epl362project/src/server/ServerTest.java
@@ -0,0 +1,132 @@
+package server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class ServerTest {
+
+	@Test
+	public void checkTables() {
+		// Open database connection
+		assertTrue("Error in getDBConnection()!", server.getDBConnection());
+		// Check if database tables exist
+		String[] tables = { "USERS", "APPOINTMENT", "CASES", "CLIENT",
+				"COMMENTED", "DISPUTE", "HISTORY", "LAWER", "LEGAL_OPINION",
+				"LEGAL_RECOMMENDATION", "MEETING", "REQUESTS", "ROLES",
+				"WARNINGS" };
+		for (String i : tables)
+			assertNotNull("Error in getResultSet()!",
+					server.getResultSet("SELECT * FROM dbo." + i));
+		// Close database connection
+		assertTrue("Error in closeDBConnection()!", server.closeDBConnection());
+	}
+
+	@Test
+	public void checkTableSyntax() {
+		// Open database connection
+		assertTrue("Error in getDBConnection()!", server.getDBConnection());
+		// Check if invalid table returns proper null result
+		String str = "WARN";
+		assertNull("Error in getResultSet() when table not exists!",
+				server.getResultSet("SELECT * FROM dbo." + str));
+		// Close database connection
+		assertTrue("Error in closeDBConnection()!", server.closeDBConnection());
+	}
+
+	@Test
+	public void checkDatabaseFunctions() {
+		// Open database connection
+		assertTrue("Error in getDBConnection()!", server.getDBConnection());
+		// Insert record
+		String[] str = {
+				"INSERT INTO dbo.USERS(USERNAME,PASSWORD,ROLE) VALUES('test','test',0)",
+				"UPDATE dbo.USERS SET ROLE = 1 WHERE USERNAME = 'test'",
+				"DELETE FROM dbo.USERS WHERE USERNAME = 'test'" };
+		for (String i : str)
+			assertTrue("Error in executeUpdate()! -> " + i,
+					(boolean) server.executeUpdate(i));
+		// Close database connection
+		assertTrue("Error in closeDBConnection()!", server.closeDBConnection());
+	}
+
+	@Test
+	public void checkSocket() throws IOException, ClassNotFoundException,
+			SQLException {
+		// Test socket connection between server / 'client' (thread)
+		String str = "BOUNCING STRING";
+		
+		// Thread to pose as client (threads used to avoid blocking accept
+		new thClient(str).start();
+
+		// Open connection
+		assertTrue("Error in openSocket()!", server.openSocket());
+		
+		// Check input / output streams
+		assertNotNull("Error cSock input stream is null!", server.cSock.getInputStream());
+		ObjectInputStream in = new ObjectInputStream(server.cSock.getInputStream());
+		assertNotNull("Error socket input stream is null!", in);
+		assertNotNull("Error cSock output stream is null!", server.cSock.getOutputStream());
+		ObjectOutputStream out = new ObjectOutputStream(server.cSock.getOutputStream());
+		assertNotNull("Error socket output stream is null!", out);
+
+		// Receive object from client
+		String Oin = (String) in.readObject();
+		assertNotNull("Error, server read NULL!", Oin);
+		
+		// Return object to client
+		out.writeObject(Oin);
+	
+		// Close connection
+		server.closeSocket();
+	}
+
+	Socket sock;
+	Object sout;
+
+	// Thread to serve as client for testing server socket
+	private class thClient extends Thread {
+		// Object to be bounced
+		Object o;
+
+		// Thread initializer object set from test
+		public thClient(Object obj) {
+			o = obj;
+		}
+
+		// Send data to server
+		private void handleSocketIn(Object q) throws UnknownHostException,
+				IOException {
+			sock = new Socket("localhost", 6789);
+			ObjectOutputStream out = new ObjectOutputStream(
+					sock.getOutputStream());
+			out.writeObject(q);
+		}
+
+		// Receive same data from server
+		private void handleSocketOut() throws IOException,
+				ClassNotFoundException {
+			ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
+			sout = in.readObject();
+			assertNotNull("Error, server responded with NULL", sout);
+			assertEquals("Error, server did not sent received obj!", o, sout);
+			sock.close();
+		}
+
+		public void run() {
+			try {
+				handleSocketIn(o);
+				handleSocketOut();
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- epl362project/src/server/server.java
+++ epl362project/src/server/server.java
@@ -13,21 +13,21 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-public class server implements Serializable{
+public class server implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private static ServerSocket sock;
-	private static Socket cSock;
+	static Socket cSock;
 	private static boolean dbDriverLoaded = false;
 	private static Connection conn = null;
-	
-	private static boolean getDBConnection() {
-		
+
+	static boolean getDBConnection() {
+
 		String dbConnString = "jdbc:sqlserver://apollo.in.cs.ucy.ac.cy:1433;databaseName=lawcs;user=lawcs;password=H9pCFzXb;";
-		
+
 		// Load the driver
 		if (!dbDriverLoaded)
 			try {
@@ -37,20 +37,33 @@ public class server implements Serializable{
 				JOptionPane.showMessageDialog(null, "Cannot load DB driver!");
 				return false;
 			}
-		
+
 		// Establish connection
 		try {
 			if (conn == null || conn.isClosed())
 				conn = DriverManager.getConnection(dbConnString);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "Cannot connect to the DB!\nGot error:" +
-			e.getErrorCode() + "\nSQL State: " + e.getSQLState() + "\nMessage: " + e.getMessage());
+			JOptionPane.showMessageDialog(null,
+					"Cannot connect to the DB!\nGot error:" + e.getErrorCode()
+							+ "\nSQL State: " + e.getSQLState() + "\nMessage: "
+							+ e.getMessage());
 			return false;
 		}
 		return true;
 	}
-	
-	private static boolean openSocket() {
+
+	static boolean closeDBConnection() {
+		try {
+			if (conn != null && !conn.isClosed())
+				conn.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	static boolean openSocket() {
 		try {
 			sock = new ServerSocket(6789);
 			cSock = sock.accept();
@@ -61,9 +74,8 @@ public class server implements Serializable{
 		}
 		return true;
 	}
-	
 
-	private static void closeSocket() {
+	static void closeSocket() {
 		if (!sock.isClosed())
 			try {
 				sock.close();
@@ -79,10 +91,8 @@ public class server implements Serializable{
 				e.printStackTrace();
 			}
 	}
-	
 
-	
-	private static ResultSet getResultSet(String str) {
+	static ResultSet getResultSet(String str) {
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
@@ -94,48 +104,48 @@ public class server implements Serializable{
 			return null;
 		}
 	}
-	
-	
-	public static Object[][] executeQuery(String query) throws SQLException{
-	    ResultSet rs = getResultSet(query);
-	    if (rs==null) return null; 
-	    ResultSetMetaData rsMetaData = rs.getMetaData();
-	    int columnCount = rsMetaData.getColumnCount();
-	    ArrayList<Object[]> result = new ArrayList<Object[]>();
-	    Object[] header = new Object[columnCount];
-	    
-	    // Get the labels
-	    for (int i=1; i <= columnCount; i++){
-	        Object label = rsMetaData.getColumnLabel(i);
-	        header[i-1] = label;
-	    }
-	    
-	    // Get the results
-	    while (rs.next()){
-	        Object[] str = new Object[columnCount];
-	        for (int i=1; i <= columnCount; i++){
-	            Object obj = rs.getObject(i);
-	            str[i-1] = obj;
-	        }
-	        result.add(str);
-	    }
-	    
-	    // Create 2D Object array from ResultSet
-	    int resultLength = result.size();
-	    Object[][] finalResult = new Object[resultLength+1][columnCount];
-	    try{
-	    	finalResult[0] = header;
-	    	for(int i=1;i<=resultLength;i++){
-		        Object[] row = result.get(i-1);
-		        finalResult[i] = row;
-		    }
-	    }catch(ArrayIndexOutOfBoundsException e){
-	    	return null;
-	    }
-	    return finalResult;
+
+	public static Object[][] executeQuery(String query) throws SQLException {
+		ResultSet rs = getResultSet(query);
+		if (rs == null)
+			return null;
+		ResultSetMetaData rsMetaData = rs.getMetaData();
+		int columnCount = rsMetaData.getColumnCount();
+		ArrayList<Object[]> result = new ArrayList<Object[]>();
+		Object[] header = new Object[columnCount];
+
+		// Get the labels
+		for (int i = 1; i <= columnCount; i++) {
+			Object label = rsMetaData.getColumnLabel(i);
+			header[i - 1] = label;
+		}
+
+		// Get the results
+		while (rs.next()) {
+			Object[] str = new Object[columnCount];
+			for (int i = 1; i <= columnCount; i++) {
+				Object obj = rs.getObject(i);
+				str[i - 1] = obj;
+			}
+			result.add(str);
+		}
+
+		// Create 2D Object array from ResultSet
+		int resultLength = result.size();
+		Object[][] finalResult = new Object[resultLength + 1][columnCount];
+		try {
+			finalResult[0] = header;
+			for (int i = 1; i <= resultLength; i++) {
+				Object[] row = result.get(i - 1);
+				finalResult[i] = row;
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return null;
+		}
+		return finalResult;
 	}
-	
-	public static Object executeUpdate(String str){
+
+	public static Object executeUpdate(String str) {
 		Statement stmt = null;
 		try {
 			stmt = conn.createStatement();
@@ -148,23 +158,25 @@ public class server implements Serializable{
 	}
 
 	public static void main(String argv[]) throws Exception {
-		
+
 		System.out.print("> Connecting with database...");
-		if (!getDBConnection()){
+		if (!getDBConnection()) {
 			System.out.println("Error!");
 			return;
 		}
 		System.out.println("Done!");
-		
+
 		System.out.println("> Waiting for client...");
 		while (true) {
 			openSocket();
-			ObjectInputStream inFromClient = new ObjectInputStream(cSock.getInputStream());
-			ObjectOutputStream outToClient = new ObjectOutputStream(cSock.getOutputStream());
-			String Oin = (String)inFromClient.readObject();
+			ObjectInputStream inFromClient = new ObjectInputStream(
+					cSock.getInputStream());
+			ObjectOutputStream outToClient = new ObjectOutputStream(
+					cSock.getOutputStream());
+			String Oin = (String) inFromClient.readObject();
 			System.out.println("> Receive: " + Oin);
-			Object Oout=null;
-			if (Oin.charAt(0)=='S' || Oin.charAt(0)=='s')
+			Object Oout = null;
+			if (Oin.charAt(0) == 'S' || Oin.charAt(0) == 's')
 				Oout = executeQuery(Oin);
 			else
 				Oout = executeUpdate(Oin);
